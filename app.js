a050d67755854c24945b77a2a3c8ba5420455b41
sufiyan <sufisk2526@gmail.com>
2024-07-29 16:41:26 +0530

    single review page

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+$(window).on("scroll load", function () {
+  if ($(window).scrollTop() > 90) {
+    $("header").addClass("header-active");
+  } else {
+    $("header").removeClass("header-active");
+  }
+});
+
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
 
