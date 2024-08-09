ef1a83d8578a28c8f0ec0e9abf3642ce92733832
sufiyan <sufisk2526@gmail.com>
2024-08-09 20:17:23 +0530

    form validation, with errors

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 
 
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  const confirmPassword = document.querySelector("#confirmPassword").value;
+  const errors = document.querySelectorAll('.formErr');
+
+
   // Signup Form
   try {
     const signupForm = document.querySelector("#signupForm");
+
     signupForm.addEventListener("submit", (event) => {
       event.preventDefault();
-      const name = document.querySelector("#name").value;
-      const email = document.querySelector("#email").value;
-      const password = document.querySelector("#password").value;
-      const confirmPassword = document.querySelector("#confirmPassword").value;
 
+      validateFrom();
       if (name && email && password && confirmPassword) {
         if (password === confirmPassword) {
           localStorage.setItem("name", name);
@@ -24,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
           alert("Passwords do not match.");
         }
       } else {
-        alert("Please fill in all fields.");
+        // alert("Please fill in all fields.");
       }
     });
   } catch (err) {
@@ -58,4 +66,95 @@ document.addEventListener("DOMContentLoaded", function () {
   } catch (err) {
     console.log(err.message);
   }
+
+  // Validation 
+
+  function clearErrors() {
+
+
+    for (let item of errors) {
+      item.innerHTML = "";
+    }
+
+
+  }
+
+
+
+  let setErr = (idx, error) => {
+
+    // elm = document.getElementById(id);
+    // console.log(errors);
+    errors[idx].innerHTML = error;
+    // errors.document.getElementsByClassName('.formErr')[0].innerHTML = error;
+
+  }
+
+  let validateFrom = () => {
+
+    let returnval = true;
+    clearErrors();
+    console.log("name",name);
+    console.log("0");
+
+    // Name 
+    if (name.length == 0) {
+      setErr("0", "Name is required");
+      returnval = false;
+    }
+
+    if (name.length < 5) {
+      setErr("0", "Length of name is too short");
+      returnval = false;
+    }
+
+    // if (!isNaN(Number(name))) {
+    //   setErr("0", "Name must only contain letters.");
+    //   returnval = false;
+    // }
+
+    if (name.length > 15) {
+      setErr("0", "Length of name is too long");
+      returnval = false;
+    }
+
+    // Email 
+    if (email.length > 15) {
+      setErr("1", "Email length is too long");
+      returnval = false;
+    }
+
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === "") {
+      setErr("1", "Email is required");
+      returnval = false;
+    }
+    else if (!emailPattern.test(email)) {
+      setErr("1", "Please enter a valid email address.");
+
+    } else {
+      setErr("1", "");
+    }
+
+    // Password 
+    if (password === "") {
+      setErr("2", "Password is required");
+      returnval = false;
+    }
+
+    if (password.length < 6) {
+      setErr("2", "Password must be at least 6 characters long");
+      returnval = false;
+    }
+
+    if(confirmPassword === "") {
+      setErr("3", "Confirm password is required");
+      returnval = false;
+    }
+  }
+
+
 });
+
+
--- js/common.js
+++ js/common.js
@@ -1,51 +1,70 @@
 
 // Scroll
 $(window).on("scroll load", function () {
-    if ($(window).scrollTop() > 90) {
-      $("header").addClass("header-active");
-    } else {
-      $("header").removeClass("header-active");
-    }
-  });
-  
-  // Menu
-  try {
-    const menu = document.querySelector(".menu");
-    const nav = document.querySelector("nav");
-  
-    menu.addEventListener("click", () => {
-      menu.classList.toggle("menu-ani");
-      nav.classList.toggle("nav-show");
-    });
-
-    document.getElementById("year").innerHTML = new Date().getFullYear();
-  } catch (err) {
-    console.log(err.message);
+  if ($(window).scrollTop() > 90) {
+    $("header").addClass("header-active");
+  } else {
+    $("header").removeClass("header-active");
   }
-  
-  
-  
-  
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const log = document.querySelector("#log");
-    const user = document.querySelector("#user");
-  
-    const checkUserStatus = () => {
-      const isLoggedIn = localStorage.getItem("isLoggedIn");
-  
-      if (isLoggedIn) {
-        if (log && user) {
-          log.style.display = "none";
-          user.style.display = "block";
-        }
-      } else {
-        if (log && user) {
-          log.style.display = "flex";
-          user.style.display = "none";
-        }
-      }
-    };
-    checkUserStatus();
+});
+
+// Menu
+try {
+  const menu = document.querySelector(".menu");
+  const nav = document.querySelector("nav");
+
+  menu.addEventListener("click", () => {
+    menu.classList.toggle("menu-ani");
+    nav.classList.toggle("nav-show");
   });
-  
+
+  document.getElementById("year").innerHTML = new Date().getFullYear();
+} catch (err) {
+  console.log(err.message);
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const log = document.querySelector("#log");
+  const user = document.querySelector("#user");
+
+  const checkUserStatus = () => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn) {
+      if (log && user) {
+        log.style.display = "none";
+        user.style.display = "block";
+      }
+    } else {
+      if (log && user) {
+        log.style.display = "flex";
+        user.style.display = "none";
+      }
+    }
+  };
+  checkUserStatus();
+});
+
+// LogOut 
+try {
+  let logout = document.querySelector("#logout");
+  logout.addEventListener("click", () => {
+    localStorage.clear();
+    window.location.reload();
+  });
+} catch (err) {
+  console.log(err.message);
+}
+// Show Name 
+try {
+  let showname = document.querySelector("#name");
+  const storedName = localStorage.getItem("name");
+  showname.innerHTML = `<span class="fw-500">Name:</span> <br> ${storedName}`;
+}
+catch (err) {
+  console.log(err.message);
+}
